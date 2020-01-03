a59b64fd6408f7d59e7b4b0207768630dcc95ec3
Marie Comet <mariecomet666@gmail.com>
2020-01-03 16:46:45 +0100

    add custom controls

--- skins/button/skin-simple.php
+++ skins/button/skin-simple.php
@@ -24,6 +24,7 @@ class Skin_Simple extends Skin_Base {
 		$this->parent = $parent;
 
 		add_filter( 'elementor/widget/print_template', array( $this, 'skin_print_template' ), 10, 2 );
+		add_action( 'elementor/element/button/section_button/before_section_end', [ $this, 'add_controls' ] );
 		add_action( 'elementor/element/button/section_button/after_section_end', [ $this, 'update_controls' ] );
 	}
 
@@ -52,6 +53,46 @@ class Skin_Simple extends Skin_Base {
 	public function get_title() {
 		return __( 'Simple', 'elementor-custom-skins' );
 	}
+
+	/**
+	 * Add skin controls
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function add_controls() {
+
+		// start injection of skin control before the Link widget control
+		$this->parent->start_injection( [
+			'at' => 'before',
+			'of' => 'link',
+		] );
+
+		$this->add_control(
+			'button_custom_attr',
+			[
+				'label' => __( 'Button attribute', 'elementor-custom-skins' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => '',
+				'options' => [
+					'' => __( 'None', 'elementor-custom-skins' ),
+					'first-attr' => __( 'First attribute', 'elementor-custom-skins' ),
+					'second-attr' => __( 'Second attribute', 'elementor-custom-skins' )
+				]
+			]
+		);
+		$this->add_control(
+			'button_custom_attr_value',
+			[
+				'label' => __( 'Attribute value', 'elementor-custom-skins' ),
+				'type' => Controls_Manager::TEXT,
+				'default' => '',
+			]
+		);
+
+		$this->parent->end_injection();
+	}
+
 
 	/**
 	 * Update parent widget controls
@@ -101,11 +142,20 @@ class Skin_Simple extends Skin_Base {
 	public function render() {
 		$settings = $this->parent->get_settings();
 
+		// get skin settings values
+		$button_custom_attr = $this->get_instance_value( 'button_custom_attr' );
+		$button_custom_attr_value = $this->get_instance_value( 'button_custom_attr_value' );
+
 		$this->parent->add_render_attribute( 'wrapper', 'class', 'elementor-button-wrapper' );
 
 		if ( ! empty( $settings['link']['url'] ) ) {
 			$this->parent->add_link_attributes( 'button', $settings['link'] );
 			$this->parent->add_render_attribute( 'button', 'class', 'elementor-button-link' );
+		}
+
+		// add button attributes from skin settings values
+		if ( ! empty( $button_custom_attr ) && ! empty( $button_custom_attr_value ) ) {
+			$this->parent->add_render_attribute( 'button', $button_custom_attr, $button_custom_attr_value );
 		}
 
 		$this->parent->add_render_attribute( 'button', 'class', 'elementor-button' );
